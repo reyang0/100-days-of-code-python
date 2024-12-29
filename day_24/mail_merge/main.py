c06e011d2d977f8120c7940371659c50ae1a7b2f
reyang0 <139074475+reyang0@users.noreply.github.com>
2024-12-28 21:11:45 -0800

    Add files via upload

---
+++ day_24/mail_merge/main.py
@@ -0,0 +1,24 @@
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: This method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+PLACEHOLDER = "[name]"
+
+with open("./Input/Letters/starting_letter.txt", "r") as file:
+    letter = file.read()
+    
+with open("./Input/Names/invited_names.txt", "r") as file:
+    names = file.readlines()
+    for name in names:
+        with open(f"./Output/ReadyToSend/letter_for_{name.strip()}.txt", "w") as file:
+            file.write(letter.replace(PLACEHOLDER, name.strip()))
+
+
+
+
+
